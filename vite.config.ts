5c014e7fd6768d04f6d8c390cb7038f713adf31f
vanshguptav111-collab <vanshguptav111@gmail.com>
2026-03-20 21:25:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 
 // https://vite.dev/config/
+export default {
+  base: '/FakeNewsDetector/'
+}
 export default defineConfig({
   base: '/FakeNewsDetector/',
   plugins: [react()],
